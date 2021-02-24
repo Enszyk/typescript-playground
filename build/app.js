642795478616e4fdc333e5912fdff8ac859f155b
Norbert Sztyk <norbert2000@onet.eu>
2021-02-24 19:51:50 +0100

    Basic types from handbook

--- build/app.js
+++ build/app.js
@@ -1,2 +1,25 @@
 "use strict";
-var isWorking = true;
+var listOfNumbers = [1, 2, 3];
+var listOfNumbers2 = [1, 2, 3];
+var tupleExample;
+tupleExample = ['hello', 1];
+tupleExample[0].toUpperCase();
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections[CardinalDirections["West"] = 1] = "West";
+    CardinalDirections[CardinalDirections["Eest"] = 2] = "Eest";
+    CardinalDirections[CardinalDirections["South"] = 3] = "South";
+    CardinalDirections[CardinalDirections["North"] = 4] = "North";
+})(CardinalDirections || (CardinalDirections = {}));
+var direction = CardinalDirections.West;
+var inWhichDirection = CardinalDirections[2];
+var notSure = 4;
+if (typeof notSure === "string") {
+    var randomString = notSure;
+}
+var alertUser = function () { return console.log("alert"); };
+function error(message) {
+    throw new Error(message);
+}
+var someValue = "this is a string";
+var strLength = someValue.length;
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,56 @@
-const isWorking = true;
+// LIST
+
+let listOfNumbers: Number[] = [1, 2, 3]
+let listOfNumbers2: Array<number> = [1, 2, 3]
+
+
+// TUPLE
+
+let tupleExample: [string, number]
+tupleExample = ['hello', 1]
+
+// Type 'number' is not assignable to type 'string'
+// tupleExample = [1, 2];
+
+tupleExample[0].toUpperCase();
+
+// Property 'toUpperCase' does not exist on type 'number'
+// tupleExample[1].toUpperCase(); 
+
+// ENUM
+
+enum CardinalDirections {
+  West = 1,
+  Eest,
+  South,
+  North
+}
+
+let direction: CardinalDirections = CardinalDirections.West;
+let inWhichDirection = CardinalDirections[2];
+
+// UNKNOWN
+
+let notSure: unknown = 4;
+
+if (typeof notSure === "string") {
+  let randomString: string = notSure;
+
+  // Type 'string' is not assignable to type 'number'
+  // let randomInt: number = notSure;
+}
+
+// VOID
+
+let alertUser = (): void => console.log("alert");
+
+// NEVER
+
+function error(message: string): never {
+  throw new Error(message);
+}
+
+// ASSERTION
+
+let someValue: unknown = "this is a string";
+let strLength: number = (someValue as string).length;
